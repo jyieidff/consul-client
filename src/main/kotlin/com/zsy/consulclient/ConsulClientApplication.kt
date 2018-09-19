38aa6a83ffca060b80d3cd3fb11a64876e2ba073
I'm <625013913@qq.com>
2018-09-19 10:36:12 +0800

    加入健康检查

--- src/main/kotlin/com/zsy/consulclient/ConsulClientApplication.kt
+++ src/main/kotlin/com/zsy/consulclient/ConsulClientApplication.kt
@@ -2,8 +2,12 @@ package com.zsy.consulclient
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class ConsulClientApplication
 
 fun main(args: Array<String>) {
